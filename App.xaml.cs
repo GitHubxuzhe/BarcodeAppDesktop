394c326ca010461713498d097464c02eee8269e8
xuzhe <68523910+GitHubxuzhe@users.noreply.github.com>
2020-10-12 12:57:55 +0800

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Globalization;
+using System.Windows;
+using BarcodeAppDesktop.Enumrations;
+using BarcodeAppDesktop.Helper;
+using BarcodeAppDesktop.Properties;
+using Exceptionless;
+using GalaSoft.MvvmLight.Messaging;
+using WPFLocalizeExtension.Engine;
+
+namespace BarcodeAppDesktop
+{
+    /// <summary>
+    ///     Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        #region Overrides of Application
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            ExceptionlessClient.Current.Register();
+            LocalizeDictionary.Instance.Culture =
+                CultureInfo.GetCultureInfo(Settings.Default.SelectedLanguage.GetDescription());
+            LocalizeDictionary.Instance.PropertyChanged +=
+                (s, args) => Messenger.Default.Send(LocalizeDictionary.Instance.Culture);
+        }
+
+
+        #region Overrides of Application
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            Settings.Default.SelectedLanguage =
+                LocalizeDictionary.Instance.Culture.TwoLetterISOLanguageName.GetValueFromDescription<LanguageEnum>();
+            Settings.Default.Save();
+            base.OnExit(e);
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
